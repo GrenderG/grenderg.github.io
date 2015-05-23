3d5b480c4db0558c6bf1561c979a3cfb2be3ae0d
GrenderG <grenderg@gmail.com>
2015-05-23 14:18:00 +0200

    new version of finder

--- assets/js/lmwebfinder.js
+++ assets/js/lmwebfinder.js
@@ -9,7 +9,7 @@
  * *******************************************************
  */
 
-primaryColor = "#FFE0B2"; //color for all matches
+primaryColor = "#FFF3E0"; //color for all matches
 secondaryColor = "#FFCC80"; //color for focused matched
 findId = "findIn"; //The text you want to search in must be inside an element with this id
 
